5a096000ecbc3741ff74500c2923b9bcc640f051
thiagocalm <thiagocalm@outlook.com>
2025-09-24 18:23:33 +0200

    updating render r code

--- material auxiliar/render_quarto.R
+++ material auxiliar/render_quarto.R
@@ -1,10 +1,10 @@
 # path
-input_path = file.path("presentations","final presentation")
+input_path = file.path(here::here(),"dia 4","slides")
 
-quarto::quarto_render(input = file.path(input_path,"edsd_FInalPresentation.qmd"))
+quarto::quarto_render(input = file.path(input_path,"4. avaliacao de modelos e logit.qmd"))
 
 pagedown::chrome_print(
-  file.path(input_path,"edsd_FInalPresentation.html"),
+  file.path(input_path,"4. avaliacao de modelos e logit.html"),
   options = list(
     printBackground = FALSE,
     preferCSSPageSize = FALSE,
